f5d122a1058037b31d9e574ca1bb5632aa1950a4
schnikonos <nicolas.jouvet@wanadoo.fr>
2024-12-26 17:47:19 +0100

    temp packaging

--- packageApp.sh
+++ packageApp.sh
@@ -1,8 +1,1 @@
-jpackage --name "MySecretSanta" --app-version "1.0" --type exe --input target --main-jar api-secret-santa-1.0.0.jar --main-class org.springframework.boot.loader.launch.JarLauncher --icon secret-santa.ico --win-shortcut --win-dir-chooser --win-menu --win-menu-group "My Apps"
-
-jpackage --name "MySecretSanta" --app-version "1.0" --type exe --input target --main-jar api-secret-santa-1.0.0.jar --main-class org.springframework.boot.loader.launch.JarLauncher --icon secret-santa.ico --win-shortcut --install-dir "%LocalAppData%\SecretSantaApp" --win-menu --win-menu-group "My Apps"
-
-jpackage --name "MySecretSanta" --app-version "1.0" --type exe --input target --main-jar api-secret-santa-1.0.0.jar --main-class org.springframework.boot.loader.launch.JarLauncher --icon secret-santa.ico --win-shortcut --win-dir-chooser --win-menu --win-menu-group "My Apps"
-
-
-jpackage --name "MySecretSanta" --app-version "1.0" --type exe --input target --main-jar api-secret-santa-1.0.0.jar --main-class org.springframework.boot.loader.launch.JarLauncher --icon secret-santa.ico --win-shortcut --win-dir-chooser --win-menu --win-menu-group "My Apps"
+jpackage --name "MySecretSanta" --app-version "1.0" --type exe --input target --main-jar api-secret-santa-1.0.0.jar --main-class org.springframework.boot.loader.launch.JarLauncher --icon secret-santa.ico --win-shortcut --win-per-user-install --win-dir-chooser --win-console --win-menu --win-menu-group "My Apps"
